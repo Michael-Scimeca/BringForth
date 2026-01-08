164c5e579983e469e2f9811fab36a770f736c9df
Michael Scimeca <7259452+mikeyscimeca@users.noreply.github.com>
2026-01-07 18:13:34 -0600

    UPdate

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="flex min-h-screen flex-col">
       <main className="flex-1 flex items-center justify-center p-4">
         <h1 className="text-4xl font-bold text-center w-full md:w-1/2">
-          We are on our way to help build the internetd.ss
+          We are on our way to help build the internetd.sss
         </h1>
       </main>
     </div>
