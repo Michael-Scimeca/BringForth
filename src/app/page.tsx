d56d37b0ad732dadabb059ef9c1a4865edf40a9e
Michael Scimeca <7259452+mikeyscimeca@users.noreply.github.com>
2026-01-07 19:08:17 -0600

    Updte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Hero from "@/components/Hero";
-import Features from "@/components/Features";
-import Contact from "@/components/Contact";
+import Hero from "@/components/sections/Hero";
+import Features from "@/components/sections/Features";
+import Contact from "@/components/sections/Contact";
 
 export default function Home() {
   return (
